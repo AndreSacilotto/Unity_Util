9e884a2e7b73c22dab8ea6f9f802ff3eabea4733
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2021-07-24 13:22:16 -0300

    Horizontal Line

---
+++ Editor/Attributes/Decorators/HorizontalLineAttributeDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Spectra.Attributes
+{
+	[CustomPropertyDrawer(typeof(HorizontalLineAttribute), false)]
+	public class HorizontalLineAttributeDecorator : DecoratorDrawer
+	{
+		public override float GetHeight()
+		{
+			var atr = attribute as HorizontalLineAttribute;
+			return EditorGUIUtility.singleLineHeight + atr.lineHeight + atr.spaceHeight;
+		}
+
+		public override void OnGUI(Rect position)
+		{
+			var atr = attribute as HorizontalLineAttribute;
+
+			Rect rect = EditorGUI.IndentedRect(position);
+
+			rect.y += GetHeight() / 3.0f;
+			rect.height = atr.lineHeight;
+			EditorGUI.DrawRect(rect, atr.lineColor);
+		}
+	}
+}
---
+++ Runtime/Attributes/Decorators/HorizontalLineAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spectra.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class HorizontalLineAttribute : PropertyAttribute
+    {
+        public readonly float spaceHeight;
+        public readonly float lineHeight;
+        public readonly Color lineColor;
+
+        public HorizontalLineAttribute(float lineHeight, float spaceHeight = 0)
+        {
+            this.spaceHeight = spaceHeight;
+            this.lineHeight = lineHeight;
+
+            lineColor = Color.gray;
+        }
+
+        public HorizontalLineAttribute(float lineHeight, float spaceHeight, float r, float g, float b)
+        {
+            this.spaceHeight = spaceHeight;
+            this.lineHeight = lineHeight;
+
+            lineColor = new Color(r, g, b);
+        }
+    }
+}
